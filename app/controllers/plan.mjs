101608600323cd2381b97dc082e0c62cc2b289b5
Agus <agustin@arla.com.ar>
2026-05-10 20:15:28 -0300

    Get ejercicios por plan

--- app/controllers/plan.mjs
+++ app/controllers/plan.mjs
@@ -25,3 +25,32 @@ export const getPlan = async (req, res) => {
     console.log(error);
   }
 };
+
+export const getPlanById = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({
+        message: "Not id valid",
+      });
+    }
+    const response = await pool.query(
+      `SELECT pe.id_plan, pe.id_ejercicio, e.detalle, e.id_tipo_ejercicio, te.detalle AS tipo_ejercicio, pe.series, pe.rep, pe.peso
+      FROM plan_ejercicio AS pe
+      INNER JOIN ejercicio AS e ON pe.id_ejercicio = e.id
+      INNER JOIN tipo_ejercicio AS te ON e.id_tipo_ejercicio = te.id
+      WHERE pe.id_plan=$1`,
+      [id],
+    );
+
+    console.log(response.rows);
+
+    res.status(200).json({
+      message: "Se obtuvieron los ejercicios exitosamente",
+      ejercicios: response.rows,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
--- app/routers/router.mjs
+++ app/routers/router.mjs
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { login } from "../controllers/auth.mjs";
-import { getPlan } from "../controllers/plan.mjs";
+import { getPlan, getPlanById } from "../controllers/plan.mjs";
 
 const router = Router();
 
 router.post("/login", login);
 
 router.get("/get/plan/:id", getPlan);
-
+router.get("/get/plan/:id/ejercicios", getPlanById);
 export default router;
